d874107e9b8eda02bc9ae2fd5e5642aded72f8e5
martinbotta <martinbotta85@gmail.com>
2024-04-15 17:44:17 -0300

    Ejercicios Clase 1 de Java

---
+++ src/main/java/VariablesDos.java
@@ -0,0 +1,14 @@
+public class VariablesDos {
+    public static void main(String[] args) {
+
+        String nombre1;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licenciaDeConducir;
+        double estaturaDeLaPersona;
+        int cantidadDeHijos;
+
+    }
+}
